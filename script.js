829ea67ec24d7c9db6155be8a29e38462708433c
Aditi505 <aditi@nineleaps.com>
2021-09-29 16:40:56 +0530

    style: Add css style

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ var billInput = document.querySelector("#bill-amt");
 
 var cashInput = document.querySelector("#cash-amt");
 
+var output = document.querySelector("#output");
+
 var numNotes = document.querySelectorAll(".noOfNotes");
 
 var notes = [2000, 500, 100, 20, 10, 5, 1];
 btnCheck.addEventListener("click", clickHandler);
 
 function clickHandler() {
-  if (billInput.value < 0) {
-    alert("Please make sure bill is greater than 0");
+  if (billInput.value === "" || cashInput.value === "") {
+    output.innerText = "Please enter valid values";
   } else {
-    if (Number(cashInput.value) < Number(billInput.value)) {
-      alert("Please make sure cash is greater than bill");
+    if (billInput.value < 0) {
+      output.innerText = "Please make sure bill is greater than 0";
     } else {
-      var remainingBalance = cashInput.value - billInput.value;
-      for (var i = 0; i < notes.length; i++) {
-        var returnNote = Math.floor(remainingBalance / notes[i]);
-        remainingBalance = remainingBalance % notes[i];
-        numNotes[i].innerText = returnNote;
+      if (Number(cashInput.value) < Number(billInput.value)) {
+        output.innerText = "Please make sure cash is greater than bill";
+      } else if (Number(cashInput.value) === Number(billInput.value)) {
+        output.innerText = "No amount to be returned";
+      } else {
+        var remainingBalance = cashInput.value - billInput.value;
+        for (var i = 0; i < notes.length; i++) {
+          var returnNote = Math.floor(remainingBalance / notes[i]);
+          remainingBalance = remainingBalance % notes[i];
+          numNotes[i].innerText = returnNote;
+        }
       }
     }
   }
